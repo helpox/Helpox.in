1e6fec701953a026e9684b535fba571ad0101852
helpox <127859715+helpox@users.noreply.github.com>
2023-04-04 19:00:30 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+const openBurger = ()=>{
+    document.getElementById('mobileburger').style.left = '0px';
+}
+
+const closeBurger = ()=>{
+ document.getElementById('mobileburger').style.left = '-100vw';
+
+}
+
